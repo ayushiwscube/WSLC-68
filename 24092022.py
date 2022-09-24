2220bc0debed845dd894d60c655d34f4be3df2fe
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-09-24 18:01:55 +0530

    Add files via upload

---
+++ 24092022.py
@@ -0,0 +1,99 @@
+#creation of a class
+
+# class hi:
+#     def hello(self):
+#         print("welcome to ws cubetech")
+#
+# a = hi()
+# a.hello()
+# b = hi()
+# b.hello()
+
+
+# class hi:
+#     v = "hello"
+# obj = hi()
+# print (obj.v)
+
+
+
+# class mobile:
+#     def features(self,cam,ram,size):
+#         print ("camera",cam)
+#         print ("RAM",ram)
+#         print ("Screen Size",size)
+# # creation of an object
+# oneplus = mobile()
+# nokia = mobile()
+#
+# choice = int(input("enter 1 or 2: "))
+# if choice == 1:
+#     oneplus.features("48MP","12GB","6 inch")
+# else:
+#     nokia.features("5MP","2GB","3 inch")
+
+
+#ticket booking system
+"""
+150
+theatre/airlines/railway/bus
+2
+normal/recliner
+pop corn combo = 200
+600
+148 seats
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pickle
+# f = open("demo.txt","rb")
+#
+# a = pickle.load(f)
+#
+# username = input("enter username: ")
+# if username in a.keys():
+#     password = input("enter password: ")
+#     if password == a[username]:
+#         print ("login successful")
+#     else:
+#         print ("incorrect password")
+# else:
+#     print ("incorrect username")
+# f.close()
+
+# if len(password)<8:
+#     print("create a strong password")
+# dict[username] = password
+#
+# pickle.dump(dict,f)
