3fcc0956a4e783751ae23de345c10c50c9728d8e
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-19 19:02:35 +0530

    Create 19102022.py

---
+++ 19102022.py
@@ -0,0 +1,84 @@
+
+
+# import numpy as np
+# arr1 = np.array([6,5,9,2])
+# arr2 = np.array([4,6,2,8])
+#
+# print(arr1**arr2)
+
+
+
+
+# import numpy as np
+# arr = np.array([[[1,2],[4,5]],[[5,6],[7,8]]])
+
+# print(arr[1][0][0])
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+#
+# arr = np.array([[[1,2],[4,5]],[[6,7],[8,9]]])
+# print(arr)
+# print(arr.ndim)
+
+
+
+
+
+
+
+
+# l = [[1,2,3],[1,2,3,4,5],[1,3]]
+# print(l)
+# import numpy as  np
+#
+# arr = np.array([[1,2,3],[1,2,3],[1,3,4]])
+# print(arr)
+
+
+
+
+
+
+
+
+# a = [1,2,3,4]
+# b = [5,6,7,8]
+#
+# c = a+b
+# print(c)
+# import numpy as np
+# arr1 = np.array([1,2,3,4])
+# arr2 = np.array([5,6,7,8])
+# print(arr1)
+
+
+
+
+
+
+
+
+
+
+
+# import pandas as pd
+#
+# dict = {"EMP 1" : ["john",23,"male"],
+#         "EMP 2": ["david",26,"male"],
+#         "EMP 3": ["lisa", 35, "female"]}
+#
+# df = pd.DataFrame(dict)
+# print(df)
+# df.loc[len(df)] = ["zida",22,"female"]
+# print(df)
+# df.drop(["EMP 1"],inplace=True, axis=1)
+# print(df)
