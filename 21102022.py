5c4253e2c7fa3c02b192ea7aa6ff47dad46899bc
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-21 19:01:36 +0530

    Create 21102022.py

---
+++ 21102022.py
@@ -0,0 +1,40 @@
+# import pandas as pd
+# import numpy as np
+#
+# dict = {"Math":[100,89,np.nan, 67],
+#         "Science":[56,78,100,87],
+#         "English":[np.nan, 67,89,np.nan]}
+#
+# df = pd.DataFrame(dict)
+# print(df)
+# ip = df.interpolate(method="linear", limit_direction="backward")
+# print(ip)
+# dr = df.replace(to_replace=np.nan, value=90)
+# print(dr)
+#print(df["English"].fillna(85))
+# print(df.fillna(method="pad"))
+# print(df.fillna(method="bfill"))
+#print((df.notnull()))
+
+
+
+
+
+# import numpy as np
+# arr = np.zeros(5)
+# print(arr)
+# arr = np.array([[2,3],[4,5]])
+# arr1 = np.array([[4,5],[6,7]])
+#
+# print(arr*arr1)
+
+# mat = np.matrix([[2,3],[4,5]])
+# mat1 = np.matrix([[4,5],[6,7]])
+#
+# print(mat*mat1)
+# print(mat.diagonal())
+# print(mat)
+# print(mat.transpose())
+# print(mat.trace())
+
+
