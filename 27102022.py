90e276aea8d2216fe7d3152c760b4805af6ff05d
ayushiwscube <113885677+ayushiwscube@users.noreply.github.com>
2022-10-27 17:59:57 +0530

    Create 27102022.py

---
+++ 27102022.py
@@ -0,0 +1,95 @@
+# def greeting():
+#     print("hello")
+#     greeting()
+#
+# greeting()
+
+# def fact(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n * fact(n-1)
+# 
+# print(fact(6))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x = 23
+#
+# def food():
+#     global x
+#     x = x + 45
+#     print(x)
+#
+# food()
+#
+# def combo():
+#     global x
+#     x = x + 45
+#     print(x)
+#
+# combo()
+
+#
+# print(x)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #000-000-0000
+# import re
+#
+# numbers = "121-343-5656    1234 515 2222   999 12a3 444"
+#
+# a = re.findall(r"[\d]{3}-[\d]{3}-[\d]{4}", numbers)
+# print(a)
+#
+# names = "John Cena   Harry potter    spider man"
+
+
+
+
+
+
+
+
+# import re
+#
+# string = "charlie and the chocolate factory"
+#
+# x = re.findall("fo", string)
+# b = re.search("ie",string)
+# a = re.split("\s",string,2)
+# y = re.sub("\s", "@", string, 1)
+# print(y)
+#print(a)
+#print(x)
